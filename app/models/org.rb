cf69ee4076588c7dad7e42176db4f03533659668
Mike Long <mblongii@gmail.com>
2016-04-23 23:02:11 -0700

    require org name; require space name

--- app/models/org.rb
+++ app/models/org.rb
@@ -1,4 +1,5 @@
 class Org < ApplicationRecord
   has_many :spaces
   has_many :members
+  validates :name, presence: true
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,5 @@
 class Space < ApplicationRecord
   belongs_to :org
   has_many :members
+  validates :name, presence: true
 end
